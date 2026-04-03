a9df4fad99acae278b74606d46c77f5ee8acc361
Sahil Kumar <83568871+sahilsujeet1@users.noreply.github.com>
2026-04-03 18:44:41 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans = "";
+        int n = word1.size(), m = word2.size();
+        int i=0;
+        
+        while(i < n || i < m) {
+            if(i < n) ans+= word1[i];
+            if(i < m) ans += word2[i];
+            i++;
+        }
+
+        return ans;
+    }
+};
